e2cd042db869c306caa37978d8cfb9da00240291
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-27 18:45:38 -0400

    Update utils.py

--- src/tesseract/utils/utils.py
+++ src/tesseract/utils/utils.py
@@ -1,5 +1,15 @@
-# utils.py
 import json
 import os
 import os.path
-Class Utils:
+from sys  import platform
+
+class Utils:
+
+    @staticmethod
+    def getOS():
+        if(platform == "linux") or(platform == "linux2"):    
+            return "linux"
+        elif(platform == "windows") or(platform == "win32"):
+            return "windows"
+        else:
+            return "unknown"
