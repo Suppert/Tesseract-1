02779fd027d24125438e1fd760fb86c9ba1255ad
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-27 18:09:46 -0400

    Update tesseract.py

--- src/tesseract/tesseract.py
+++ src/tesseract/tesseract.py
@@ -1,4 +1,15 @@
 # tesseract.py
 #!/usr/bin/env python
+from pyraklib.server import PyRakLibServer
+from pyraklib.server import ServerHandler
+import sys
+import os
+from tesseract.resources import Config
 
-Class Tesseract:
+# STARTUP TAKEN FROM THE SPIFFY PROJECT
+config = config.handle_config()
+server = PyRakLibServer(config['port'])
+serverInstance = Handler()
+handler = ServerHandler(server, serverInstance)
+handler.sendOption("name", "MCPE;" + config['name'] + ";82 82;0.15.4;0;20")
+print("starting server on *:" + str(config['port']))
